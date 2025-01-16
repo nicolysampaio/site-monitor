8469383ed2ec4cde6486ffee2576045e7219e455
Nicoly Sampaio <nicoly.sampaioluz@gmail.com>
2025-01-16 00:14:53 -0300

    feat: implement continuous monitoring feature in hello.go

--- hello/hello.go
+++ hello/hello.go
@@ -2,26 +2,30 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 	"os"
 )
 
 func main() {
 	showIntroduction()
-	showMenu()
 
-	option := scanOption()
+	for {
+		showMenu()
 
-	switch option {
-	case 1:
-		fmt.Println("Monitoring...")
-	case 2:
-		fmt.Println("Displaying logs...")
-	case 0:
-		fmt.Println("Exiting program...")
-		os.Exit(0)
-	default:
-		fmt.Println("Invalid command")
-		os.Exit(-1)
+		option := scanOption()
+
+		switch option {
+		case 1:
+			startMonitoring()
+		case 2:
+			fmt.Println("Displaying logs...")
+		case 0:
+			fmt.Println("Exiting program...")
+			os.Exit(0)
+		default:
+			fmt.Println("Invalid command")
+			os.Exit(-1)
+		}
 	}
 }
 
@@ -46,4 +50,16 @@ func scanOption() int {
 	fmt.Println("The chosen option was", scannedOption)
 
 	return scannedOption
+}
+
+func startMonitoring() {
+	fmt.Println("Monitoring...")
+	site := "https://httpbin.org/status/404"
+	resp, _ := http.Get(site)
+
+	if resp.StatusCode == 200 {
+		fmt.Println("Site:", site, "loaded with success!")
+	} else {
+		fmt.Println("Site:", site, "failed to load. Status Code:", resp.StatusCode)
+	}
 }
